559b83444a6d0cd594437f6229cc7643616f3911
vinayakshivanandhadaginal <vinayakhadaginal7@gmail.com>
2026-01-04 10:23:27 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-  document.querySelectorAll(".section").forEach(section => {
-    const pos = section.getBoundingClientRect().top;
-    if (pos < window.innerHeight - 100) {
-      section.style.opacity = 1;
-      section.style.transform = "translateY(0)";
-    }
-  });
-});
